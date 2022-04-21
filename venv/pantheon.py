baa79b22118e06eaa9aa7b93d557d92153718580
stromanp <stromanp@queensu.ca>
2022-04-21 10:38:09 -0400

    updates from laptop

--- venv/pantheon.py
+++ venv/pantheon.py
@@ -289,7 +289,7 @@ class BaseFrame:
 
         # create a label under the pictures (row 2), spanning two columns, to tell the user what they are running
         # specify a black background and white letters, with 12 point bold font
-        self.L0 = tk.Label(self.parent, text = "PANTHEON: whole CNS fMRI Analysis", bg = bgcol, fg = fgcol1, font = "none 14 bold")
+        self.L0 = tk.Label(self.parent, text = "PANTHEON:  whole CNS fMRI Analysis", bg = bgcol, fg = fgcol1, font = "none 14 bold")
         self.L0.grid(row=1, column = 0, columnspan = 2, sticky = 'W')
 
 
