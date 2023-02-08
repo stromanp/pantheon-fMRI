e28d104fe61da4c07aa2969e3ddfbe7b46f6274a
stroman_desktop <stromanp@queensu.ca>
2023-02-08 10:02:33 -0500

    updates from desktop

--- venv/pantheon.py
+++ venv/pantheon.py
@@ -82,6 +82,21 @@ if os.name == 'nt':
     widgetbg = 'SystemButtonFace'
     mainbg = 'SystemButtonFace'
 else:
+    # old version --  colours for Mac/Linux
+    fgcol1 = 'red'
+    fgcol2 = 'red'
+    fgcol3 = 'red'
+    bgcol = 'grey94'
+    fgletter1 = 'black'
+    fgletter2 = 'black'
+    fgletter3 = 'black'
+    widgetfont = "none 9 bold"
+    widgetfont2 = "none 9 bold"
+    labelfont = "none 9 bold"
+    radiofont = "none 9"
+    infofont = "none 9"
+    widgetbg = '#3E4149'
+
     # colours for Mac/Linux
     fgcol1 = 'red'
     fgcol2 = 'red'
@@ -113,6 +128,7 @@ else:
     infofont = "none 9"
     widgetbg = '#808080'
     mainbg = '#fefefe'
+    mainbg = '#6a0000'
 
 bigbigbuttonsize = 21
 bigbuttonsize = 14
